c2f0e93074c9b09e5a37f5e65dc81ce564986e5c
Nitish Rathi <nitishrathi@gmail.com>
2019-03-30 07:40:17 +0000

    Inventory deserves have its own file

--- lib/goby/entity/entity.rb
+++ lib/goby/entity/entity.rb
@@ -1,54 +1,6 @@
 require 'goby'
 
 module Goby
-  class Inventory
-    include Enumerable
-    extend Forwardable
-
-    def_delegators :@items, :each, :delete, :push, :empty?, :size, :length, :[], :sample
-
-    def initialize(items)
-      @items = items
-    end
-
-    def entry(item)
-      @items.detect { |couple| couple.first.name.casecmp?(item.to_s) }
-    end
-
-    def find_item(item)
-      entry(item)&.first
-    end
-
-    def add_item(item, amount = 1)
-      found = entry(item)
-      if found
-        found.second += amount
-      else
-        @items.push(C[item, amount])
-      end
-    end
-
-    def clear
-      @items = []
-    end
-
-    def remove_item(item, amount = 1)
-      couple = entry(item)
-      if couple
-        couple.second -= amount
-        @items.delete(couple) if couple.second <= 0
-      end
-    end
-
-    def format_items
-      @items.sum('') { |couple| "\n* #{couple.first.name} (#{couple.second})" }
-    end
-
-    def ==(other)
-      @items == other
-    end
-  end
-
   # Provides the ability to fight, equip/unequip weapons & armor,
   # and carry items & gold.
   class Entity
